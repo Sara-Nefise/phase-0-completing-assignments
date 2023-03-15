7da5770847be18a72f1f57cf275e7bcac9285178
ms.sara <sara_nafisa@protonmail.com>
2023-03-15 21:18:36 +0300

    A message

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@ const jsdom = require( 'jsdom' );
 const path = require( 'path' );
 var chai = require( 'chai' );
 chai.use( require( 'chai-fs' ) );
-
+//hello
 describe( 'This assignment', () => {
   it( 'has been correctly cloned to your local environment', () => {
     chai.assert.isDirectory( './.git', 'no ".git" folder was found within "welcome-completing-assignment". Use "git init" to create one' );
